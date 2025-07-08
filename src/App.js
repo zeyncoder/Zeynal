2fdefaf9069bc8f41f0be21366e06a8dcd2f75d2
Zeynal <zeynal@Zeynals-MacBook-Air.local>
2025-07-08 20:22:36 +0400

    version1

--- src/App.js
+++ src/App.js
@@ -2,24 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1>Welcome to TheZeynal Platform</h1>
+                <p>Güclü ideyalar. Gələcək yönümlü həllər.</p>
+
+                <a
+                    className="App-link"
+                    href="https://github.com/TheZeynal"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    GitHub Profilinə Keçid
+                </a>
+
+                <a
+                    className="App-link"
+                    href="https://www.youtube.com/@TheZeynalOfficial"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    YouTube Kanalına Bax
+                </a>
+
+                <a
+                    className="App-link"
+                    href="https://www.youtube.com/watch?v=ldLa-Cqdl_E"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    🎵 The Zeyn -X dinle
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
